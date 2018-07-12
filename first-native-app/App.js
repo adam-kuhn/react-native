29214baf66597d71d719bf917459d4433646174f
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-12 09:01:06 +0300

    4-53 connect to Redux Dev Tools

--- first-native-app/App.js
+++ first-native-app/App.js
@@ -1,13 +1,15 @@
 // allows to not right React.Component for extends ...
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 // react-native is a bunch of components that will be compiled to native code (browsers don't understnat these, and native applications don't understand <div> etc.,)
 import {StyleSheet, View} from 'react-native'
 
 import List from './src/components/List/List'
 import PlaceInput from './src/components/PlaceInput/PlaceInput'
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail'
+import {addPlace, deletePlace, selectPlace, deselectPlace} from './src/store/actions/index'
 // import placeImage from './src/assets/cool-poke.jpg'
-export default class App extends Component {
+class App extends Component {
 state = {
   placeName: '',
   places: [],
@@ -24,57 +26,29 @@ state = {
     if (this.state.placeName.trim() === '') {
       return
     }
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random().toString(),
-          name: prevState.placeName,
-          // when image is imported it stored as a JS object that can be rendered on the app, not just a path
-          // image: placeImage
-          image: {
-            // image address
-            // images loaded over the internet must be styled
-            uri: 'https://www.planwallpaper.com/static/images/Cool-HD-Wallpapers-1_9j9pfu8.jpg'
-          }
-        })
-      }
-    })
+    this.props.onAddPlace(this.state.placeName)
   }
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter((place) => {
-          return place.key !== prevState.selectedPlace.key
-        }),
-        selectedPlace: null
-      }
-    })
+    this.props.onDeletePlace()
   }
 
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    })
+    this.props.onDeselectPlace()
   }
   placeSelected = (key) => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => {
-          return place.key === key
-        })
-      }
-    })
+    this.props.onSelectPlace(key)
   }
+
   render () {
     return (
       <View style={styles.container}>
-        <PlaceDetail selectedPlace={this.state.selectedPlace}
+        <PlaceDetail selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler}/>
         <PlaceInput handleChange={this.placeNameChangedHandler}
           handleSubmit={this.placeSubmitHandler}
           name={this.placeName} />
-        <List listOfPlaces={this.state.places} onItemSelected={this.placeSelected}/>
+        <List listOfPlaces={this.props.places} onItemSelected={this.placeSelected}/>
       </View>
     )
   }
@@ -89,3 +63,20 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start'
   }
 })
+
+const mapStateToProps = (state) => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAddPlace: (name) => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: (key) => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
