77437bc6f768935e25f0550d67cbc907cdc717dd
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-13 17:21:24 +0300

    6-71: tabs have icons and now work with android

--- first-native-app/src/screens/MainTabs/startMainTabs.js
+++ first-native-app/src/screens/MainTabs/startMainTabs.js
@@ -1,19 +1,30 @@
 import {Navigation} from 'react-native-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: 'awesome-places.FindPlaceScreen',
-        label: 'Find Place',
-        title: 'Find Place'
-      },
-      {
-        screen: 'awesome-places.SharePlaceScreen',
-        label: 'Share Place',
-        title: 'Share Place'
-      }
-    ]
+  // Icon.getImageSource returns a promise. So we need to use Promise.All to make sure all icons are recieved, before moving to actually rendering the tabs
+  //   Icon.getImageSource('img', 30(size), color)
+  Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('ios-share-alt', 30)
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: 'awesome-places.FindPlaceScreen',
+          label: 'Find Place',
+          title: 'Find Place',
+          icon: sources[0]
+        },
+        {
+          screen: 'awesome-places.SharePlaceScreen',
+          label: 'Share Place',
+          title: 'Share Place',
+          icon: sources[1]
+
+        }
+      ]
+    })
   })
 }
 
