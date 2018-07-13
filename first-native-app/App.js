0ab289691ef651002cc1e952da8c3a987e011a1a
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-13 17:13:39 +0300

    6-70 add tabs, but need to use icons for android

--- first-native-app/App.js
+++ first-native-app/App.js
@@ -1,13 +1,20 @@
 import {Navigation} from 'react-native-navigation'
 
 import AuthScreen from './src/screens/Auth/Auth.js'
+import SharePlaceScreen from './src/screens/MainTabs/SharePlace/SharePlace'
+import FindPlaceScreen from './src/screens/MainTabs/FindPlace/FindPlace'
 // Register Screens
-Navigation.registerComponent('awesome-places/AuthScreen', () => AuthScreen)
+// naming convention is App name '.' ScreenName
+// Screens are how we navigate on react-native. Think of as pages our routes on the web
+// import all screens in this app File
+Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen)
+Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen)
+Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen)
 
 // Start a App
 Navigation.startSingleScreenApp({
   screen: {
-    screen: 'awesome-places/AuthScreen',
+    screen: 'awesome-places.AuthScreen',
     title: 'Login'
   }
 })
