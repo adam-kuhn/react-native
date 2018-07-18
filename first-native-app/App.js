7b47ea5c25fcbbb0bffa12e03d4b599470187856
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-18 09:35:56 +0300

    6-72 begin connectin screens to redux

--- first-native-app/App.js
+++ first-native-app/App.js
@@ -1,15 +1,21 @@
 import {Navigation} from 'react-native-navigation'
+import {Provider} from 'react-redux'
 
 import AuthScreen from './src/screens/Auth/Auth.js'
 import SharePlaceScreen from './src/screens/MainTabs/SharePlace/SharePlace'
 import FindPlaceScreen from './src/screens/MainTabs/FindPlace/FindPlace'
+// configureStore, is the same code from App.js
+import configureStore from './src/store/configureStore'
+
+const store = configureStore()
 // Register Screens
 // naming convention is App name '.' ScreenName
 // Screens are how we navigate on react-native. Think of as pages our routes on the web
 // import all screens in this app File
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen)
-Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen)
-Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen)
+// to connect to redux pass Provider, and store to the screen. If redux is not needed don't worry about passing anything
+Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, store, Provider)
+Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen, store, Provider)
+Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen, store, Provider)
 
 // Start a App
 Navigation.startSingleScreenApp({
