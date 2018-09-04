7c90850ef28b50395f85c2c6b7af85ec3ea86e52
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-04 10:42:36 +0300

    add background image to auth

--- first-native-app/src/screens/Auth/Auth.js
+++ first-native-app/src/screens/Auth/Auth.js
@@ -1,29 +1,34 @@
 import React, {Component} from 'react'
-import {View, Text, Button, StyleSheet} from 'react-native'
+import {View, ImageBackground, Button, StyleSheet} from 'react-native'
 
 import startMaintabs from '../MainTabs/startMainTabs'
 import DefaultInput from '../../components/UI/Defaultinput'
 import HeadingText from '../../components/UI/HeadingText'
 import MainText from '../../components/UI/MainText'
+import backgroundImg from '../../assets/background.jpg'
+
 class AuthScreen extends Component {
   loginHandler = () => {
     startMaintabs()
   }
   render () {
     return (
-      <View style={styles.container}>
-        <MainText>
-          <HeadingText>Please Login</HeadingText>
-        </MainText>
-        <Button title="Switch to Login" />
-        <View style={styles.inputContainer}>
-          {/* underlineColorAndroid=transparent gets rid of the defaul line that appears under the inputs in android */}
-          <DefaultInput placeholder='Your Email address' style={styles.input}/>
-          <DefaultInput placeholder='Password' style={styles.input} />
-          <DefaultInput placeholder='Confirm Password' style={styles.input} />
+      // when using ImageBackground have to set the width
+      <ImageBackground source={backgroundImg} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Please Login</HeadingText>
+          </MainText>
+          <Button title="Switch to Login" />
+          <View style={styles.inputContainer}>
+            {/* underlineColorAndroid=transparent gets rid of the defaul line that appears under the inputs in android */}
+            <DefaultInput placeholder='Your Email address' style={styles.input}/>
+            <DefaultInput placeholder='Password' style={styles.input} />
+            <DefaultInput placeholder='Confirm Password' style={styles.input} />
+          </View>
+          <Button title='Submit' onPress={this.loginHandler}/>
         </View>
-        <Button title='Submit' onPress={this.loginHandler}/>
-      </View>
+      </ImageBackground>
     )
   }
 }
@@ -34,6 +39,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  backgroundImage: {
+    width: '100%',
+    flex: 1
   },
   // need to set input width otherwise it will take up smallest amount of space possible which is essentially zero
   // careful with hardcoding values i.e width: 300, because this may be larger than some devices
