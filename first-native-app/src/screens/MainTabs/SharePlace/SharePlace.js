24da76e22a12688a99c0a3bccdbfe352452bc753
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-05 14:43:12 +0300

    complete basic styling

--- first-native-app/src/screens/MainTabs/SharePlace/SharePlace.js
+++ first-native-app/src/screens/MainTabs/SharePlace/SharePlace.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react'
-import {View, Text, TextInput, Button, StyleSheet, ScrollView} from 'react-native'
+import {View, Text, TextInput, Button, StyleSheet, ScrollView,
+  Image} from 'react-native'
 import {connect} from 'react-redux'
 
 import {addPlace} from '../../../store/actions/index'
+import DefaultInput from '../../../components/UI/Defaultinput'
+import MainText from '../../../components/UI/MainText'
+import HeadingText from '../../../components/UI/HeadingText'
+import imagePreview from '../../../assets/cool-poke.jpg'
+
 class SharePlaceScreen extends Component {
   // toggle drawer code below ---
   constructor (props) {
@@ -32,13 +38,25 @@ class SharePlaceScreen extends Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-          <Text>Share a place!</Text>
-          <View style={styles.placeHolder}><Text>Image Preview</Text></View>
-          <Button title='Pick Image' />
-          <View style={styles.placeHolder}><Text>Map</Text></View>
-          <Button title='Locate Me' />
-          <TextInput placeholder='Place Name' />
-          <Button title='Share the Place!' />
+          <MainText>
+            <HeadingText>Share a place!</HeadingText>
+          </MainText>
+          <View style={styles.placeHolder}>
+            <Image source={imagePreview} style={styles.previewImage}/>
+          </View>
+          <View style={styles.button}>
+            <Button title='Pick Image' />
+          </View>
+          <View style={styles.placeHolder}>
+            <Text>Map</Text>
+          </View>
+          <View style={styles.button}>
+            <Button title='Locate Me' />
+          </View>
+          <DefaultInput placeholder='Place Name' />
+          <View style={styles.button}>
+            <Button title='Share the Place!' />
+          </View>
         </View>
       </ScrollView>
     )
@@ -56,6 +74,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#eee',
     width: '80%',
     height: 150
+  },
+  button: {
+    margin: 8
+  },
+  previewImage: {
+    width: '100%',
+    height: '100%'
   }
 })
 
